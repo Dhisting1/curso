b5d26cd3b5003ecb9556e018970960c6eb850e27
Lucas Fernando Gonlçaves Lima <lucas.lima@estudante.ifb.edu.br>
2023-05-09 21:03:43 -0300

    atualização-to-do-list

--- JavaScript/Curso/To do List/js/todolist.js
+++ JavaScript/Curso/To do List/js/todolist.js
@@ -5,36 +5,70 @@
   const itemInput = document.getElementById("item-input");
   const todoAddForm = document.getElementById("todo-add");
   const ul = document.getElementById("todo-list");
-  const lis = ul.getElementsByTagName("li");
+  // const lis = ul.getElementsByTagName("li");
 
-  function addTask(task) {
+  // Estrutura de dados que vai armazenar o objeto, onde cada propriedade vai representar uma tarefa
+
+  let arrTasks = [
+    {
+      name: "Exemplo",
+      createAt: Date.now(),
+      completed: false,
+    },
+  ];
+
+  function addEventLi(li) {
+    li.addEventListener("click", function () {
+      console.log(this);
+    });
+  }
+
+  // Função que recebe um objeto (arrTasks) e vai retornar uma <li> preparada (com classes e textos) adicionar na <ul>
+
+  function generateLiTask(obj) {
     const li = document.createElement("li");
     const p = document.createElement("p");
     li.classList.add("todo-item");
     p.classList.add("task-name");
-    p.textContent = task;
+    p.textContent = obj.name;
     li.appendChild(p);
-    ul.appendChild(li);
 
-    li.addEventListener("click", function () {
-      console.log(this);
-      console.log(this.textContent);
-      console.log(this.innerText);
-      console.log(this.innerHTML);
-      console.log(this.outerHTML);
+    addEventLi(li);
+
+    return li;
+  }
+
+  // Vai renderizar na tela, onde primeiro vai limpar a tela e depois acrescenta as li novamente
+
+  function renderTasks() {
+    ul.innerHTML = "";
+
+    arrTasks.forEach((taskObj) => {
+      ul.appendChild(generateLiTask(taskObj));
+      //Primeiro ele gera a <li> com a função generateLiTask depois ele adiciona na ul
     });
   }
+
+  // Vai adicionar uma tarefa/objeto na array arrTasks
+
+  function addTask(task) {
+    arrTasks.push({
+      name: task,
+      createAt: Date.now(),
+      completed: false,
+    });
+  }
+
+  // Evento de submit no formulario e quando for submetido vai adicionar um objeto novo a partir da chamada da função addTasks e depois vai renderizar com a função renderTasks
 
   todoAddForm.addEventListener("submit", (e) => {
     e.preventDefault();
     addTask(itemInput.value);
+    renderTasks();
+
     itemInput.value = "";
     itemInput.focus();
   });
 
-  ul.addEventListener("click", function (evt) {
-    if (evt.target.classList.contains("todo-item")) {
-      console.log(evt.target);
-    }
-  });
+  renderTasks();
 })();
